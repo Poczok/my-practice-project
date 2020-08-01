d841e38122f49c873a71a1dd0652a9f0357d90d2
Pecze Tamás <tamas.pecze@pctrade.hu>
2020-08-01 18:27:39 +0200

    minor refactor

--- src/app/modules/modal-list/components/modal/modal-list.component.ts
+++ src/app/modules/modal-list/components/modal/modal-list.component.ts
@@ -8,23 +8,31 @@ import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
   selector: 'app-modal-list',
   templateUrl: './modal-list.component.html',
   styleUrls: ['./modal-list.component.scss'],
-  providers: [ModalService, BasicDataService]
+  providers: [ModalService, BasicDataService],
 })
 @UntilDestroy()
 export class ModalListComponent implements OnInit {
-
   private _clickCounter = 0;
   private _raceResults: IRaceTable[];
-  constructor(private _modalService: ModalService, private _basicDataService: BasicDataService) { }
+  constructor(
+    private _modalService: ModalService,
+    private _basicDataService: BasicDataService
+  ) {}
 
   ngOnInit(): void {
-    this._basicDataService.getRaceResults().pipe(untilDestroyed(this)).subscribe(raceResults => {
-      this._raceResults = raceResults;
-    })
+    this._basicDataService
+      .getRaceResults()
+      .pipe(untilDestroyed(this))
+      .subscribe((raceResults) => {
+        this._raceResults = raceResults;
+      });
   }
   openModal() {
     console.log('open');
     this._clickCounter++;
-    this._modalService.openTestDialog(this._clickCounter.toString(), this._raceResults);
+    this._modalService.openTestDialog(
+      this._clickCounter.toString(),
+      this._raceResults
+    );
   }
 }
